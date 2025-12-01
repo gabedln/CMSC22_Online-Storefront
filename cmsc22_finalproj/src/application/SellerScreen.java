1dc4dd5cb048a03883b483943b9332e816642205
gabedln <182726586+gabedln@users.noreply.github.com>
2025-12-01 17:16:38 +0800

    sellerScreen updated logic and files

---
+++ cmsc22_finalproj/src/application/SellerScreen.java
@@ -0,0 +1,39 @@
+package application;
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import user.Seller;
+
+public class SellerScreen {
+	
+	private Scene seller;
+	
+	public SellerScreen(Stage stage, Seller seller) {
+		Button startSelling = new Button("start selling");
+		startSelling.setMinWidth(450);
+		startSelling.getStyleClass().add("startSelling-button");
+		
+		BorderPane root = new BorderPane();
+		root.getStyleClass().add("sellerscreen_initial");
+		Scene sellerScene = new Scene(root, 1024, 576);
+		sellerScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		this.seller = sellerScene;
+		
+		VBox button = new VBox(startSelling);
+		button.setStyle("-fx-padding: 0 0 120 15;");
+		button.setAlignment(Pos.CENTER);
+		
+		if(seller.getProducts().size()==0) {
+			root.getStyleClass().add("sellerscreen_initial");
+			root.setBottom(button);
+		}
+	}
+	
+	public Scene getScene() { return this.seller; }
+	
+	
+}
