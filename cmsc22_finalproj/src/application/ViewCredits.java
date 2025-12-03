fdd786f5adc250a71c65ddc36362180bf3c19785
Avigail Ann A. Monsalud <aamonsalud@up.edu.ph>
2025-12-03 17:47:05 +0800

    Update ViewCredits.java

--- cmsc22_finalproj/src/application/ViewCredits.java
+++ cmsc22_finalproj/src/application/ViewCredits.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
@@ -15,25 +16,25 @@ public class ViewCredits {
     public ViewCredits(Stage stage, Scene previous) {
         BorderPane root = new BorderPane();
 
+        Button backButton = new Button("back");
+        backButton.getStyleClass().add("back-button-main");
+        backButton.setOnAction(e -> stage.setScene(previous));
+
+        HBox topBar = new HBox(backButton);
+        topBar.setAlignment(Pos.TOP_LEFT);
+
         // Fullscreen background image
         Image creditsImage = new Image(getClass().getResourceAsStream("/application/images/credits.png"));
         ImageView creditsView = new ImageView(creditsImage);
         creditsView.setFitWidth(1024);
         creditsView.setFitHeight(576);
-        creditsView.setPreserveRatio(false); // stretch to cover entire screen
+        creditsView.setPreserveRatio(false);
 
-        root.setCenter(creditsView);
+        // StackPane to layer back button above image
+        StackPane layered = new StackPane(creditsView, topBar);
+        StackPane.setAlignment(topBar, Pos.TOP_LEFT);
 
-        // Back button (small, top-left corner)
-        Button backButton = new Button("back");
-        backButton.getStyleClass().add("back-button");
-        backButton.setOnAction(e -> stage.setScene(previous));
-
-        HBox topBar = new HBox(backButton);
-        topBar.setAlignment(Pos.TOP_LEFT);
-        topBar.setStyle("-fx-padding: 10;");
-
-        root.setTop(topBar);
+        root.setCenter(layered);
 
         Scene scene = new Scene(root, 1024, 576);
         scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
